13408a947c90af0e50c310cacf2ba1df0eb6b85e
aleadinglight <huutam.ptnk@gmail.com>
2018-07-13 05:29:49 +0700

    modify index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,32 +1,36 @@
-var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
-var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
-var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
-
-var recognition = new SpeechRecognition();
-var speechRecognitionList = new SpeechGrammarList();
-
-recognition.lang = 'en-US';
-recognition.interimResults = false;
-recognition.maxAlternatives = 1;
-
-var synth = window.speechSynthesis;
-var msg = new SpeechSynthesisUtterance();
-var voices = window.speechSynthesis.getVoices();
-msg.voice = voices[10]; // Note: some voices don't support altering params
-msg.voiceURI = 'native';
-msg.text = "Sorry, I did not catch that!";
-msg.lang = 'en-US';
-
-msg.onend = function(e) {
-    console.log('Finished in ' + event.elapsedTime + ' seconds.');
-};
+if (annyang) {
+    // Let's define a command.
+    var commands = {
+      'hello': function() { alert('Hello world!'); }
+    };
+  
+    // Add our commands to annyang
+    annyang.addCommands(commands);
+  
+    // Start listening.
+    annyang.start();
+}
 
 var talk = new Vue({
     el:'#app',
     methods:{
         startTalking: function(){
-            synth.speak(msg);
-            console.log();
+        }
+    }
+});
+
+var talk = new Vue({
+    el:'#app',
+    methods:{
+        startTalking: function(){
+        }
+    }
+});
+
+var talk = new Vue({
+    el:'#app',
+    methods:{
+        startTalking: function(){
         }
     }
 });
