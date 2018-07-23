fec20d09acd59c60d0e8180c4251ddd646e32948
aleadinglight <huutam.ptnk@gmail.com>
2018-07-24 01:23:03 +0700

    listening mode on

--- src/index.js
+++ src/index.js
@@ -37,3 +37,19 @@ Galadriel.addCommands([
     },
 ]);
 
+Galadriel.initialize({
+    lang: "en-GB", // GreatBritain english
+    continuous: true, // Listen forever
+    soundex: true,// Use the soundex algorithm to increase accuracy
+    debug: true, // Show messages in the console
+    executionKeyword: "and do it now",
+    listen: true, // Start to listen commands !
+
+    // If providen, you can only trigger a command if you say its name
+    // e.g to trigger Good Morning, you need to say "Jarvis Good Morning"
+    name: "Galadriel" 
+}).then(() => {
+    console.log("Artyom has been succesfully initialized");
+}).catch((err) => {
+    console.error("Artyom couldn't be initialized: ", err);
+});
