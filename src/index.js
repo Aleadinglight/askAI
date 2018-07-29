8ae2af1f37ffaf932394dd4bec666ee1d6eadf9d
aleadinglight <huutam.ptnk@gmail.com>
2018-07-29 17:37:07 +0700

    added pre tab so it can overflow

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const Galadriel = new Artyom();
 var ai = new Vue({
     el:'#ai',
     data:{
-        message: 'Hello',
+        message: 'Hellooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo',
         show:true
     },
     components: {
